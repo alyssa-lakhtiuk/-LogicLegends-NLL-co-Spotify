4ba3eeab6869b9175580cd36becca7722e334098
Anton <antonkko888@gmail.com>
2023-06-20 18:05:19 +0300

    added api/questionWithAnswers that returns json with question and all its possible answers

--- repository/api.routes.js
+++ repository/api.routes.js
@@ -3,8 +3,16 @@ const router = new Router()
 const questController = require('./quest.controller')
 
 router.get('/quest', questController.getQuests)
-router.get('/question/:questId', questController.getQuestionsForQuest)
+
+// http://localhost:3000/api/questions?questId={id of quest} returns just questions of quest
+router.get('/questions', questController.getQuestionsForQuest)
+
+// http://localhost:3000/api/questionWithAnswers?questId={id of quest}?questionNum={number of question in quest 1..4}
+// returns question with answers
+router.get('/questionWithAnswers', questController.getQuestionWithAnswers)
+
 router.get('/answer/:questionId', questController.getAnswerForQuestion)
+
 router.get('/final/:questId', questController.getFinalsForQuest)
 
 module.exports = router
